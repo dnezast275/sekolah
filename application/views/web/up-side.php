d370aabbb831f18bdf1b28a9007e26b96ac896f9
dnezast275 <muhamadramdani.h275@gmail.com>
2020-07-16 16:29:39 +0700

    Add card properties for headmaster section

--- application/views/web/up-side.php
+++ application/views/web/up-side.php
@@ -1,3 +1,12 @@
-<div class="col-lg-4" style="border: 1px solid black;"></div>
+<div class="col-lg-3" style="border: 1px solid black;">
+    <div class="card" style="width: 100%;">
+        <img src="<?= base_url('asset/img/'); ?>avatar.png" class="card-img-top" alt="Headmaster Profile">
+        <div class="card-body">
+            <h5 class="card-title">Sambutan Kepala Sekolah</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, maxime.</p>
+            <a href="#" class="btn btn-primary">Baca...</a>
+        </div>
+    </div>
+</div>
 </div>
 </div>
