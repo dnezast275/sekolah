cbddd891f82599e1d7d3342e032cf6ec468a28e5
dnezast275 <muhamadramdani.h275@gmail.com>
2020-07-17 11:11:12 +0700

    Add dummy content and layout

---
+++ application/views/web/home.php
@@ -0,0 +1,58 @@
+<section class="content mt-3">
+    <div class="container-xl">
+        <div class="row ml-3 mr-3">
+            <div class="col-lg-9">
+                <div class="row artikel mb-3">
+                    <div class="col-4">
+                        <img src="<?= base_url('asset/img/uploads/'); ?>photo1.png" alt="photo1" class="img-content">
+                    </div>
+                    <div class="col-8">
+                        <h5 class="head-artikel">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing.
+                        </h5>
+                        <i class="fa fa-clock"></i><span> 14 Juli 2020</span>
+                        <i class="fa fa-user"> </i><span> Administrator</span>
+                        <i class="fa fa-tags"> </i><span> Kategori</span>
+                        <p class="ringkasan">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis animi at placeat, harum dicta reprehenderit earum quasi totam esse ipsam modi ad!
+                        </p>
+                        <a href="#" class="read-artikel"><button class="btn btn-primary">Lanjut baca...</button></a>
+                    </div>
+                </div>
+
+                <div class="row artikel mb-3">
+                    <div class="col-4">
+                        <img src="<?= base_url('asset/img/uploads/'); ?>photo1.png" alt="photo1" class="img-content">
+                    </div>
+                    <div class="col-8">
+                        <h5 class="head-artikel">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing.
+                        </h5>
+                        <i class="fa fa-clock"></i><span> 14 Juli 2020</span>
+                        <i class="fa fa-user"> </i><span> Administrator</span>
+                        <i class="fa fa-tags"> </i><span> Kategori</span>
+                        <p class="ringkasan">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis animi at placeat, harum dicta reprehenderit earum quasi totam esse ipsam modi ad!
+                        </p>
+                        <a href="#" class="read-artikel"><button class="btn btn-primary">Lanjut baca...</button></a>
+                    </div>
+                </div>
+
+                <div class="row artikel mb-3">
+                    <div class="col-4">
+                        <img src="<?= base_url('asset/img/uploads/'); ?>photo1.png" alt="photo1" class="img-content">
+                    </div>
+                    <div class="col-8">
+                        <h5 class="head-artikel">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing.
+                        </h5>
+                        <i class="fa fa-clock"></i><span> 14 Juli 2020</span>
+                        <i class="fa fa-user"> </i><span> Administrator</span>
+                        <i class="fa fa-tags"> </i><span> Kategori</span>
+                        <p class="ringkasan">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis animi at placeat, harum dicta reprehenderit earum quasi totam esse ipsam modi ad!
+                        </p>
+                        <a href="#" class="read-artikel"><button class="btn btn-primary">Lanjut baca...</button></a>
+                    </div>
+                </div>
+            </div>
