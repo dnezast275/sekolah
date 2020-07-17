f11b8a8315c05c939a3f7e48559761c4fcd8adc8
dnezast275 <muhamadramdani.h275@gmail.com>
2020-07-17 11:09:14 +0700

    Delete dropdown javascript properties

--- application/views/front/footer.php
+++ application/views/front/footer.php
@@ -1,15 +1,20 @@
-    <script src="<?= base_url('asset/js/'); ?>jquery.min.js"></script>
-    <script src="<?= base_url('asset/bootstrap/js/'); ?>bootstrap.min.js"></script>
-    <script src="<?= base_url('asset/fontawesome/js/'); ?>fontawesome.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('.dropdown-submenu a.dropdown-item').on("click", function(e) {
-                $(this).next('ul').toggle();
-                e.stopPropagation();
-                e.preventDefault();
-            });
-        });
-    </script>
-    </body>
+</div>
+</section>
 
-    </html>
+<footer class="footer mt-3">
+    <div class="container-xl">
+        <div class="row mr-3 ml-3">
+            <div class="col-lg-4" style="border:1px solid black; height:200px"></div>
+            <div class="col-lg-4" style="border:1px solid black; height:200px"></div>
+            <div class="col-lg-4" style="border:1px solid black; height:200px"></div>
+        </div>
+    </div>
+</footer>
+
+<script src="<?= base_url('asset/js/'); ?>jquery.min.js"></script>
+<script src="<?= base_url('asset/bootstrap/js/'); ?>bootstrap.min.js"></script>
+<script src="<?= base_url('asset/fontawesome/js/'); ?>fontawesome.min.js"></script>
+
+</body>
+
+</html>
